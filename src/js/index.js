9147a5b0364441eb2790f381cfe7dbbb59fce90d
Mikolka <wilshere10000@gmail.com>
2022-11-21 13:27:57 +0300

    fix:  main index.js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,27 @@
 import { discount, discount2 } from "./utils/tools";
 import { createImageComponent } from "./components/createElements/index";
+import { createCardItemComponent } from "./components/createCard/cardItem";
+
 let itemProperty = {
   id: "x",
-  imageURL: "https://www.onliner.by",
+  imageUrl: "http://loremflickr.com/640/480/cats",
   inBasket: false,
-  discount: discount,
+  discount: 10,
+  priceValue: 1000,
+  name: "x",
+  alt: "x",
 };
+let source = [itemProperty];
 console.log(discount);
 console.log(discount2);
 
 const root = document.querySelector(".root");
 
-root.append(image);
+function loadTodos(cardDataList) {
+  cardDataList.forEach((data) => {
+    const item = createCardItemComponent(data);
+    root.append(item);
+  });
+}
+loadTodos(source);
+// root.append(createCardItemComponent(itemProperty));
