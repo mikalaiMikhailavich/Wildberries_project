1ef61f0d4a1d133bc94eca6560f688dcb2231a8f
Mikolka <wilshere10000@gmail.com>
2022-11-21 10:58:54 +0300

    add: cardItem structure

--- src/js/components/createCard/cardItem.js
+++ src/js/components/createCard/cardItem.js
@@ -7,8 +7,7 @@ import {
 } from "../createElements/index";
 
 export const createCardItemComponent = (data) => {
-  const { id, name, inBasket, src, discount, price } = data;
-
+  const { id, name, inBasket, src, alt, discount, priceValue } = data;
   const card = createContainerComponent({
     id,
     className: inBasket ? "card card__inBasket" : "card",
@@ -25,7 +24,42 @@ export const createCardItemComponent = (data) => {
     value: "Быстрый просмотр",
   });
 
-  card.append(image, buttonOpenCard, closeBtn, dateLabel);
+  const discountValue = renderElement("div", {
+    innerHTML: `${discount} % `,
+    className: "card__discount",
+  });
+
+  const buttonToBasket = createButtonElement({
+    className: inBasket ? "button button__inBasket" : "button",
+    value: inBasket ? "В корзине" : "Добавить в корзину",
+  });
+
+  const discountValueAndButtonToBasketContainer = createContainerComponent({
+    children: [discountValue, buttonToBasket],
+  });
+
+  const priceDiscount = renderElement({
+    innerHTML: `${priceValue - priceValue * discount}`,
+    className: "card__price",
+  });
+
+  const price = renderElement("div", {
+    innerHTML: priceValue,
+    className: "card__price card__price_crossed-out",
+  });
+
+  const cardName = renderElement("div", {
+    innerHTML: name,
+  });
+
+  card.append(
+    image,
+    buttonOpenCard,
+    discountValueAndButtonToBasketContainer,
+    priceDiscount,
+    price,
+    cardName
+  );
 
   return card;
 };
