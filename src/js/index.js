91392478f6ddf2bd9a9e0adc2fecb832886879cd
mikalaiMikhailavich <wilshere10000@gmail.com>
2022-11-24 01:20:38 +0300

    add: fetch and request

--- src/js/index.js
+++ src/js/index.js
@@ -2,54 +2,56 @@ import { discount, discount2 } from "./utils/tools";
 import { createImageComponent } from "./components/createElements/index";
 import { createCardItemComponent } from "./components/createCard/cardItem";
 import { createInputComponent } from "./components/createElements/input";
-let itemProperty = {
-  id: "x",
-  imageUrl: "http://loremflickr.com/640/480/cats",
-  inBasket: false,
-  discount: 10,
-  priceValue: 1000,
-  name: "x",
-  alt: "x",
-};
-let source = [
-  itemProperty,
-  itemProperty,
-  itemProperty,
-  itemProperty,
-  itemProperty,
-  itemProperty,
-  itemProperty,
-  itemProperty,
-  itemProperty,
-  itemProperty,
-];
+
+// let itemProperty = {
+//   id: "x",
+//   imageUrl: "http://loremflickr.com/640/480/cats",
+//   inBasket: false,
+//   discount: 10,
+//   priceValue: 1000,
+//   name: "x",
+//   alt: "x",
+// };
+
+const url = "https://637e6ac15b1cc8d6f92d8fb8.mockapi.io/card";
+const getOptions = { method: "GET" };
+
+const getRequest = new Request(url, getOptions);
+let source = [];
+
+fetch(getRequest)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((data) => source.push(data));
+    loadCards(source);
+  });
+
 console.log(discount);
 console.log(discount2);
-
 const root = document.querySelector(".root");
 
-function loadTodos(cardDataList) {
+function loadCards(cardDataList) {
   cardDataList.forEach((data) => {
     const item = createCardItemComponent(data);
     root.append(item);
   });
 }
-loadTodos(source);
+
 // root.append(createCardItemComponent(itemProperty));
 
 // const input = createInputComponent({ placeholder: "ewwadas" });
-const image = document.createElement("img");
-image.src = new URL("../assets/icons/basket.svg", import.meta.url);
-import svgBasket from "bundle-text:../assets/icons/basket.svg";
-const svgi = document.createElement("div");
-svgi.innerHTML = svgBasket;
-svgi.style.stroke = "red";
-svgi.style.background = "blue";
-svgi.style.background = "blue";
-svgi.setAttribute("fill", "orange");
-root.append(svgi);
-// root.append(image);
+// const image = document.createElement("img");
+// image.src = new URL("../assets/icons/basket.svg", import.meta.url);
+// import svgBasket from "bundle-text:../assets/icons/basket.svg";
+// const svgi = document.createElement("div");
+// svgi.innerHTML = svgBasket;
+// svgi.style.stroke = "red";
+// svgi.style.background = "blue";
+// svgi.style.background = "blue";
+// svgi.setAttribute("fill", "orange");
+// root.append(svgi);
+// // root.append(image);
 
-let path = document.querySelector("path");
-path.removeAttribute("fill");
-console.log(path);
+// let path = document.querySelector("path");
+// path.removeAttribute("fill");
+// console.log(path);
